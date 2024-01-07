7a04cabe064802937313d1f4516cd47436e13494
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-01-07 16:21:01 +0600

    test data added

---
+++ complexity_test.go
@@ -0,0 +1,14 @@
+package complexity_test
+
+import (
+	"testing"
+
+	complexity "github.com/chlorineCtrl/Complexity-Analyzer"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, complexity.Analyzer, []string{"a", "halstead"}...)
+}
--- go.mod
+++ go.mod
@@ -11,5 +11,4 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
